8379648a803ddf2665c5f5a70ad8943638fcad7b
Illia Kvashuk <illiakvashuk@gmail.com>
2023-12-09 14:53:26 +0200

    Added to output original text frequency

--- youtubevideofreq.py
+++ youtubevideofreq.py
@@ -19,28 +19,36 @@ def get_transcript(link):
     transcript_merged = " ".join(transcript_lines)
     return transcript_merged
 
-def get_frequency(words, start_year = 2018, end_year = 2019, smoothing = 0, corpus = "en-US-2019"):
+def get_frequency(text, start_year = 2018, end_year = 2019, smoothing = 0, corpus = "en-US-2019"):
+    words = clean_text(text)[0]
+    text_df = clean_text(text)[1]
     words = urllib.parse.quote(words)
     url = 'https://books.google.com/ngrams/json?content=' + words + '&year_start=' + str(start_year) + '&year_end=' + str(end_year) + '&corpus=' + str(corpus) + '&smoothing=' + str(smoothing) + '' 
     response = requests.get(url) 
     outputs = response.json() 
     freq = {}
     freq = {output['ngram']: np.max([i for i in output['timeseries'] if i!=0]) for output in outputs}
-    df = pd.DataFrame(freq.items(), columns= ["Word", "Frequency"])
-    return df.sort_values(by = "Frequency", ascending=False)
+    df = pd.DataFrame(freq.items(), columns= ["Word", "Language Frequency"])
+    sorted_df = df.sort_values(by = "Language Frequency", ascending=False)
+    sorted_df["Language Order"] = range(0, len(df))
+    combined_text_language_df = sorted_df.join(text_df.set_index("Word"), on = 'Word')
+    return combined_text_language_df.set_index('Word')
 
 def clean_text(words):
-    pattern = r'[{}]'.format(re.escape(string.punctuation))
-    cleaned_text = re.sub(pattern, " ", words)
+    pattern_cleaning = r'[{}]'.format(re.escape(string.punctuation))
+    cleaned_text = re.sub(pattern_cleaning, " ", words)
     cleaned_text = cleaned_text.lower().split()
     cleaned_text = [lemmatizer.lemmatize(word) for word in cleaned_text]
-    cleaned_text = set(cleaned_text)
-    cleaned_text = ",".join(cleaned_text)
-    return cleaned_text
+    unique_words = set(cleaned_text)
+    string_unique_words = ",".join(unique_words)
+    sample_size = len(cleaned_text)
+    words_df = pd.DataFrame([(word, cleaned_text.count(word)/sample_size) for word in unique_words], columns= ['Word', 'Text Frequency'])
+    words_df = words_df.sort_values(by = "Text Frequency")
+    words_df["Order"] = range(0, len(words_df))
+    return string_unique_words, words_df
 
 if __name__ == '__main__':
     link = str(sys.argv[1])
     text = get_transcript(link)
-    cleaned_text = clean_text(text)
-    feq = get_frequency(cleaned_text)
+    feq = get_frequency(text)
     print(feq.to_string())
